e460d038815da82a82923cf6b9d84802944b6166
CJHAUSER-skibidi <1019628@rochesterschools.org>
2025-05-09 09:08:23 -0500

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -3,3 +3,4 @@ const SUPABASE_URL = 'https://nrkddggevbxqdpckduxs.supabase.co'; // 👈 Replace
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5ya2RkZ2dldmJ4cWRwY2tkdXhzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDY3OTg4MjQsImV4cCI6MjA2MjM3NDgyNH0.kVzzE-uWr3ovSMyIHUNElVPvO12j2f2ZRrG7GhkxjkE';               // 👈 Replace this too
 
 const supabase = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+console.log("Supabase client initialized:", supabase);
